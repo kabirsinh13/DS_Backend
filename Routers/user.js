7d8bc8180b94d29d70f6f768e5f889c0d91dad77
Subham Kumar <ssshubhamshrivastav010@gmail.com>
2023-10-26 16:52:17 +0530

    added show user's liked post

--- Routers/user.js
+++ Routers/user.js
@@ -4,18 +4,17 @@ const router = express.Router()
 
 const cors = require('cors')
 const User = require('../Schema/user.js')
-const Like = require('../Schema/likes.js')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const auth = require('../auth/auth.js')
 
+const auth = require('../auth/auth.js')
 
 router.use(cors())
 
 router.post('/user/signup',async (req,res)=>{
     const user = new User({...req.body})
     const response =  await user.save()
-    const token = jwt.sign({_id:response._id.toString()},process.env.JWT_KEY,{expiresIn:'7 days'})
+    const token = jwt.sign({_id:response._id.toString()},process.env.JWT_KEY,{expiresIn:'6 minutes'})
     response.tokens = response.tokens.concat({token});
     await response.save()
     res.send(response)
@@ -38,7 +37,7 @@ router.post('/user/login',async (req,res)=>{
    if(isMatch){
 
        //generating token for logged in user
-       const token = jwt.sign({_id:user._id.toString()},process.env.JWT_KEY,{expiresIn:'60 minutes'})
+       const token = jwt.sign({_id:user._id.toString()},process.env.JWT_KEY,{expiresIn:'5 minutes'})
 
        //Storing token in database
        user.tokens = user.tokens.concat({token});
@@ -50,8 +49,6 @@ router.post('/user/login',async (req,res)=>{
    else{
        res.status(401).send("wrong password");
    }
-})
-
 
 router.post('/user/logout',auth,async (req,res)=>{
     try{
@@ -70,8 +67,13 @@ router.post('/user/logout',auth,async (req,res)=>{
         res.status(401).send("Logout Failed")
     }
    })
-
 })
+
+router.get('/user/likedpost',auth,async (req,res)=>{
+    //it will return all like collection which were liked by this user
+    const userLiked = await Like.find({userId:req.user._id}).populate('postId')
+    res.send(userLiked)
+ })
 
 
 module.exports = router
